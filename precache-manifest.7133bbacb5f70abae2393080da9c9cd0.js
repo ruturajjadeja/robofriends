95cd4acd99b618ebe940da5c54331ace63a96333
Marvara Jhala <marvara@Marvaras-MacBook-Pro.local>
2019-08-28 14:15:39 -0400

    Updates

---
+++ precache-manifest.7133bbacb5f70abae2393080da9c9cd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b32026147fc5b8ef556aaec2e3cdea1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "694520b8281aafd7300d",
+    "url": "/robofriends/static/css/main.ee72f1c0.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "694520b8281aafd7300d",
+    "url": "/robofriends/static/js/main.aca2039a.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
